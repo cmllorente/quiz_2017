a510e4763bdb660d11e7279bc566f898bf03a9c7
Cristina Llorente <Cris@MacBook-Air-de-Cristina.local>
2017-06-03 15:32:56 +0200

    Despliegue en heroku

--- routes/index.js
+++ routes/index.js
@@ -30,10 +30,10 @@ router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-router.get('/quizzes/random_play',	   quizController.randomplay);
-router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/random_play',	   quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
 
 module.exports = router;
