f322e1ed220bce5a28f1cbfb79f59f692132927b
Cristina Llorente <Cris@MacBook-Air-de-Cristina.local>
2017-06-06 09:14:34 +0200

    practica53

--- routes/index.js
+++ routes/index.js
@@ -138,15 +138,15 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     tipController.destroy);
 
 
-router.get('/quizzes',                     quizController.index);
-router.get('/quizzes/:quizId(\\d+)',       quizController.show);
-router.get('/quizzes/new',                 quizController.new);
-router.post('/quizzes',                    quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+//router.get('/quizzes',                     quizController.index);
+//router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+//router.get('/quizzes/new',                 quizController.new);
+//router.post('/quizzes',                    quizController.create);
+//router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+//router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+//router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+//router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+//router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 router.get('/quizzes/random_play',	   quizController.randomplay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
